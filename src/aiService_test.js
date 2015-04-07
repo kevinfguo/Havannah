6939e47d855ea8c91a8a224458b070478ce85b46
sonali-malik <sonalimalik.ms@gmail.com>
2015-04-07 19:38:04 -0400

    reordering karma.conf part2

--- src/aiService_test.js
+++ src/aiService_test.js
@@ -14,11 +14,11 @@ describe("aiService", function() {
   var horIndex = [[0, 8], [0, 9], [0, 10], [0, 11], [0, 12], [0, 13],[0,14],[0,15],
                   [1,15], [2, 15], [3, 15], [4, 15], [5, 15],[6,15],[7,15]];
 function setBoard(){
-    var i, j;
+    
     var board = new Array(14);
-    for(i=0; i<15; ++i){
+    for(var i=0; i<15; ++i){
       board[i] = new Array(14);
-      for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
+      for(var j=horIndex[i][0]; j<horIndex[i][1]; ++j){
         //  console.log(i,j);
         board[i][j] = '';
       }
@@ -26,13 +26,13 @@ function setBoard(){
     return board;
   }
 
-  iit("R finds an immediate winning move", function() {
+  it("R finds an immediate winning move", function() {
     var board=setBoard();
     for(var i=0; i<7; ++i) {
       board[0][i]='R';
     }
     var boardNext=setBoard();
-    for(var i=0; i<8; ++i) {
+    for( i=0; i<8; ++i) {
       boardNext[0][i]='R';
     }
     var move = _aiService.createComputerMove(
@@ -65,14 +65,14 @@ function setBoard(){
         board, 0, {maxDepth: 2});
     expect(angular.equals(move[2].set.value, {row: 0, col: 7})).toBe(true);
   });
-
+/*
   it("B prevents an immediate win", function() {
        
    var board4=setBoard();
-    for(i=0; i<9; ++i) {
+    for(var i=0; i<9; ++i) {
       board4[i][1]='R';
     }
-    for(i=1; i<14; ++i) {
+    for( i=1; i<14; ++i) {
       board4[8][i]='R';
     }
     var move = _aiService.createComputerMove(
@@ -82,8 +82,8 @@ function setBoard(){
 
   it("B prevents another immediate win", function() {
     var board5=setBoard();
-    for(i=1; i<4; ++i) {
-      for(j=1; j<4; ++j) {
+    for(var i=1; i<4; ++i) {
+      for(var j=1; j<4; ++j) {
         board5[i][j]='R';
       }
     }
@@ -94,7 +94,7 @@ function setBoard(){
     expect(angular.equals(move[2].set.value, {row: 1, col: 1})).toBe(true);
   });
 
- /* it("R finds a winning move that will lead to winning in 2 steps", function() {
+ it("R finds a winning move that will lead to winning in 2 steps", function() {
     var move = _aiService.createComputerMove(
         [['R', '', ''],
          ['B', 'R', ''],
