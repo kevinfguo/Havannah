8057745b4d2bb964083518a98245ad41dc8bb47b
sonali-malik <sonalimalik.ms@gmail.com>
2015-05-02 18:17:46 -0400

    adding cache5

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,9 +83,11 @@ module.exports = function(grunt) {
           basePath: '.',
           cache: [
             'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js',
-            'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css',
             'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-touch.min.js',
-            'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.1/ui-bootstrap-tpls.min.js',
+'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.1/ui-bootstrap-tpls.min.js',
+'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css',
+'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/fonts/glyphicons-halflings-regular.woff',
+'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/fonts/glyphicons-halflings-regular.ttf',
             'dist/everything.min.js',
             'game.css',
             'http://yoav-zibin.github.io/emulator/dist/turnBasedServices.2.min.js',
