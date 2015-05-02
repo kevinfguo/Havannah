291ce1c432435c367c4e48ce44e60587c37b2224
sonali-malik <sonalimalik.ms@gmail.com>
2015-05-02 02:34:03 -0400

    carousal4

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,6 +83,9 @@ module.exports = function(grunt) {
           basePath: '.',
           cache: [
             'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js',
+            'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css',
+            'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-touch.min.js',
+            'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.1/ui-bootstrap-tpls.min.js',
             'dist/everything.min.js',
             'game.css',
             'http://yoav-zibin.github.io/emulator/dist/turnBasedServices.2.min.js',
