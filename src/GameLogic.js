97ae7524bf904c971b9fb31311857bf77aeac5c5
sonali-malik <sonalimalik.ms@gmail.com>
2015-05-04 21:38:39 -0400

    changing ai poss moves2

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -614,13 +614,35 @@ ret.push([row,col]);
 		var possibleMoves =null;
 		var i, j;
 			var boardCopy ;
+		
+		var rowInd = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+		var shuffleArray = function(array) {
+			  var m = array.length, t, i;
+
+			  // While there remain elements to shuffle
+			  while (m) {
+			    // Pick a remaining element…
+			    i = Math.floor(Math.random() * m--);
+
+			    // And swap it with the current element.
+			    t = array[m];
+			    array[m] = array[i];
+			    array[i] = t;
+			  }
+
+			  return array;
+			}
+		rowInd = shuffleArray(rowInd);
+
 		for(i=14; i>-1; --i){
+
 			for(j=horIndex[i][0]; j<horIndex[i][1]; ++j){
 				try {
 					//boardCopy = angular.copy(board);
-					var randomRow = Math.floor(Math.random()*i);
-					var randomCol = Math.floor(Math.random()*j);
-					possibleMoves=createMove(board, randomRow, randomCol, turnIndexBeforeMove);
+					
+					var randomCol = Math.random() * (horIndex[i][1] - horIndex[i][0]) + horIndex[i][0];
+					possibleMoves=createMove(board, rowInd[i], randomCol, turnIndexBeforeMove);
 					return possibleMoves;
 				} catch (e) {
 					// The cell in that position was full.
